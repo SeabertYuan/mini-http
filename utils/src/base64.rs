1fc7cd0867d8cc63bd422b23aa39d1ad571634ab
SeabertYuan <seabert.s.yuan23z@gmail.com>
2025-04-27 16:58:15 -0700

    feat!: sha1 encoding

--- utils/src/base64.rs
+++ utils/src/base64.rs
@@ -1,6 +1,6 @@
 const ALPHABET: [char; 64] = [
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'N', 'M', 'O', 'P', 'Q', 'R', 'S',
-    'T', 'U', 'V', 'W', 'x', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
     '5', '6', '7', '8', '9', '+', '/',
 ];
@@ -9,43 +9,45 @@ pub fn encode(input: String) -> String {
     let in_bytes = input.as_bytes();
     let n_bytes = input.len();
     let n_groups = n_bytes / 3;
-    let res_size = n_groups * 4 + (n_bytes % 3);
-    let mut res = Vec::with_capacity(res_size);
+    let res_size = (n_bytes + 2) / 3 * 4;
+    let mut res = vec![0; res_size];
+    println!("{}", input);
     for i in 0..n_groups {
         let mut leftover: u8 = 0;
         for j in 0..3 {
             let idx: usize = i * 4 + j;
             let curr_char = in_bytes[i * 3 + j];
-            if leftover / 0b1111 > 1 {
+            if j > 1 {
                 res[idx] = ALPHABET[(leftover << 2 | (curr_char >> 6)) as usize] as u8;
                 res[idx + 1] = ALPHABET[(curr_char & 0b111111) as usize] as u8;
                 leftover = 0;
-            } else if leftover / 0b11 > 1 {
+            } else if j > 0 {
                 res[idx] = ALPHABET[((leftover << 4) | (curr_char >> 4)) as usize] as u8;
                 leftover = curr_char & 0b1111;
             } else {
-                res[idx] = ALPHABET[((leftover << 6) | (curr_char >> 2)) as usize] as u8;
+                res[idx] = ALPHABET[(curr_char >> 2) as usize] as u8;
                 leftover = curr_char & 0b11;
             }
         }
     }
-    let part_group = n_bytes % 3;
+    let part_group = 3 - n_bytes % 3;
     match part_group {
         1 => {
-            res[n_bytes - 4] = ALPHABET[(in_bytes[n_bytes - 2] >> 2) as usize] as u8;
-            res[n_bytes - 3] = ALPHABET
+            println!("{}", res_size);
+            res[res_size - 4] = ALPHABET[(in_bytes[n_bytes - 2] >> 2) as usize] as u8;
+            res[res_size - 3] = ALPHABET
                 [(((in_bytes[n_bytes - 2] & 0b11) << 4) | (in_bytes[n_bytes - 1] >> 4)) as usize]
                 as u8;
-            res[n_bytes - 2] = ALPHABET[((in_bytes[n_bytes - 1] & 0b1111) << 2) as usize] as u8;
-            res[n_bytes - 1] = b'=';
+            res[res_size - 2] = ALPHABET[((in_bytes[n_bytes - 1] & 0b1111) << 2) as usize] as u8;
+            res[res_size - 1] = b'=';
         }
         2 => {
-            res[n_bytes - 4] = ALPHABET[(in_bytes[n_bytes - 1] >> 2) as usize] as u8;
-            res[n_bytes - 3] = ALPHABET[((in_bytes[n_bytes - 1] & 0b11) << 4) as usize] as u8;
-            res[n_bytes - 2] = b'=';
-            res[n_bytes - 1] = b'=';
+            res[res_size - 4] = ALPHABET[(in_bytes[n_bytes - 1] >> 2) as usize] as u8;
+            res[res_size - 3] = ALPHABET[((in_bytes[n_bytes - 1] & 0b11) << 4) as usize] as u8;
+            res[res_size - 2] = b'=';
+            res[res_size - 1] = b'=';
         }
-        _ => unreachable!(),
+        _ => (),
     }
     // TODO make htis saafe
     String::from_utf8(res).unwrap()
@@ -55,7 +57,8 @@ pub fn encode(input: String) -> String {
 mod tests {
     use super::*;
 
-    fn expect_enc_short() {
+    #[test]
+    fn encode_short() {
         let test1 = String::from("Hello World!");
         let test2 = String::from("I love Sohee!");
         let test3 = String::from("Today was an excellent day");
@@ -66,9 +69,31 @@ mod tests {
         assert_eq!(encode(test4), "YXBwbGU=");
     }
 
-    fn exp_enc_padding() {
-        let perfect = "abc";
-        let offbyeight = "ab"; // TODO
-        let offbysixteen = "a"; // TODO
+    #[test]
+    fn encode_padding() {
+        let perfect = String::from("abc");
+        let offbyeight = String::from("ab");
+        let offbysixteen = String::from("a");
+        assert_eq!(encode(perfect), "YWJj");
+        assert_eq!(encode(offbyeight), "YWI=");
+        assert_eq!(encode(offbysixteen), "YQ==");
+    }
+
+    #[test]
+    fn encode_complex() {
+        let special = String::from("\\$*!)(@#*$&%^&");
+        let special2 = String::from("\\r\\n\\tsl~`1234567890{}|/.,<>?");
+        assert_eq!(encode(special), "XCQqISkoQCMqJCYlXiY=");
+        assert_eq!(encode(special2), "XHJcblx0c2x+YDEyMzQ1Njc4OTB7fXwvLiw8Pj8=");
+    }
+
+    #[test]
+    fn encode_long() {
+        let the_raven = std::fs::read_to_string("test_resources/the_raven.txt").unwrap();
+        let expected = std::fs::read_to_string("test_resources/b64_raven.txt").unwrap();
+        assert_eq!(
+            encode(String::from(the_raven.trim_end())),
+            expected.trim_end()
+        );
     }
 }
--- utils/src/sha1.rs
+++ utils/src/sha1.rs
@@ -1,3 +1,110 @@
 pub fn hash(input: String) -> String {
-    String::from("")
+    let mut h0: u32 = 0x67452301;
+    let mut h1: u32 = 0xEFCDAB89;
+    let mut h2: u32 = 0x98BADCFE;
+    let mut h3: u32 = 0x10325476;
+    let mut h4: u32 = 0xC3D2E1F0;
+    let in_len: u64 = input.len() as u64;
+    // padding (input length + 8 bytes from length + 1 byte for 1 bit) to align to 512 bits
+    let pad_len: u64 = 64 - (in_len + 9) % 64;
+    let total_len = pad_len + in_len + 1;
+    let mut m_bytes: Vec<u8> = Vec::with_capacity((total_len + 8) as usize);
+    for c in input.as_bytes() {
+        m_bytes.push(*c);
+    }
+    m_bytes.push(0x80);
+    while m_bytes.len() < total_len as usize {
+        m_bytes.push(0);
+    }
+    for i in 0..8 {
+        m_bytes.push(((in_len >> (7 - i) * 8) & 0xff) as u8);
+    }
+    let n_chunks = (total_len + 8) / 8;
+    for i in 0..n_chunks {
+        let mut hash_word: [u32; 80] = [0; 80];
+        for j in 0..15 {
+            hash_word[j] = ((m_bytes[4 * j] as u32) << 24) as u32
+                | ((m_bytes[4 * j + 1] as u32) << 16) as u32
+                | ((m_bytes[4 * j + 2] as u32) << 8) as u32
+                | (m_bytes[4 * j + 3]) as u32;
+        }
+        for j in 16..80 {
+            hash_word[j] = leftrotate(
+                hash_word[j - 3] ^ hash_word[j - 8] ^ hash_word[j - 14] ^ hash_word[j - 16],
+                1,
+            );
+        }
+        let mut hv0: u32 = h0;
+        let mut hv1: u32 = h1;
+        let mut hv2: u32 = h2;
+        let mut hv3: u32 = h3;
+        let mut hv4: u32 = h4;
+        let mut hv5: u32 = 0;
+        let mut hv6: u32 = 0;
+
+        for j in 0..80 {
+            if j < 20 {
+                hv5 = (hv1 & hv2) | (!hv1 & hv3);
+                hv6 = 0x5A827999;
+            } else if j < 40 {
+                hv5 = hv1 ^ hv2 ^ hv3;
+                hv6 = 0x6ED9EBA1;
+            } else if j < 60 {
+                hv5 = (hv1 & hv2) ^ (hv1 & hv3) ^ (hv2 & hv3);
+                hv6 = 0x8F1BBCDC;
+            } else {
+                hv5 = hv1 ^ hv2 ^ hv3;
+                hv6 = 0xCA62C1D6;
+            }
+            let temp: u32 = leftrotate(hv0, 5)
+                .wrapping_add(hv5)
+                .wrapping_add(hv4)
+                .wrapping_add(hv6)
+                .wrapping_add(hash_word[j]);
+            hv4 = hv3;
+            hv3 = hv2;
+            hv2 = leftrotate(hv1, 30);
+            hv1 = hv0;
+            hv0 = temp;
+        }
+        h0 = h0.wrapping_add(hv0);
+        h1 = h1.wrapping_add(hv1);
+        h2 = h2.wrapping_add(hv2);
+        h3 = h3.wrapping_add(hv3);
+        h4 = h4.wrapping_add(hv4);
+    }
+    hash_chunks_to_string([h0, h1, h2, h3, h4])
+}
+
+/// Rotate a u32 left by the amount specified.
+fn leftrotate(n: u32, amount: u8) -> u32 {
+    let msb = n >> (32 - amount);
+    (n << amount) | msb
+}
+
+fn hash_chunks_to_string(chunks: [u32; 5]) -> String {
+    let mut res: [u8; 20] = [0; 20];
+    for i in 0..5 {
+        for j in (0..4).rev() {
+            res[i * 4 + (3 - j)] = ((chunks[i] >> 8 * j) & 0xff) as u8;
+        }
+    }
+    // TODO fix the unwrap
+    String::from_utf8(res.to_vec()).unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn hash_short() {
+        let test1 = String::from("hello");
+        let test2 = String::from("let's get rusty");
+        let test3 = String::from("");
+        let test4 = String::from("Kim");
+        assert_eq!(hash(test1), "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d");
+        assert_eq!(hash(test2), "a2590e2ad169b79e91c4c8fcc804f7769d8d7f2c");
+        assert_eq!(hash(test3), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
+        assert_eq!(hash(test4), "83db02e1cba58c43d01116c50014913b47fa473b");
+    }
 }
